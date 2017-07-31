e6a636c3898ee93e93b857604a80b0d78aa1d23e
Suzanne Aitchison <suzanne.aitchison@gmail.com>
2017-07-31 15:46:28 +0100

    creates basic layout for MovieDetail view

--- app/src/main/java/com/example/android/movies/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/movies/MovieDetailActivity.java
@@ -5,7 +5,10 @@ import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -16,6 +19,7 @@ public class MovieDetailActivity extends AppCompatActivity {
     private TextView mSynopsis;
     private TextView mReleaseDate;
     private TextView mRating;
+    private ImageView mPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         mSynopsis = (TextView) findViewById(R.id.tv_movie_synopsis);
         mReleaseDate = (TextView) findViewById(R.id.tv_movie_release_date);
         mRating = (TextView) findViewById(R.id.tv_movie_rating);
+        mPoster = (ImageView) findViewById(R.id.iv_movie_poster);
 
         Intent intent = getIntent();
         Bundle b = intent.getExtras();
@@ -46,5 +51,15 @@ public class MovieDetailActivity extends AppCompatActivity {
         mSynopsis.setText(mCurrentMovie.getSynopsis());
         mReleaseDate.setText(mCurrentMovie.getReleaseDate());
         mRating.setText(String.valueOf(mCurrentMovie.getRating()));
+        populatePoster();
+    }
+
+    private void populatePoster(){
+        Picasso.with(this)
+                .load(mCurrentMovie.getPosterPath())
+                .placeholder(R.drawable.ic_image_placeholder)
+                .error(R.drawable.ic_broken_img)
+                .fit()
+                .into(mPoster);
     }
 }
